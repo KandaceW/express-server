9dcf98e102810e45a675953c729eb43aa55f36f3
Quentin Papera <49025456+quentin-papera@users.noreply.github.com>
2019-05-31 11:42:00 +1200

    dan&q

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const express = require('express')
 const server = express()
 
 module.exports = server
+
+const path = require('path')
+
+server.get('/compliment', function(req, res){
+    res.send('Sup')
+})
+
+server.get('/profile', function(req, res){
+    res.sendFile(path.join(__dirname, 'silvia.html'))
+})
+
+
+
