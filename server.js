85bc6caed46a93e011decf4e0c7c346fbca360be
Ben Sherwood <48200039+ben-sherwood@users.noreply.github.com>
2019-05-31 11:32:26 +1200

    1st commit

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express')
 
 const server = express()
+var PORT = 3000
+
+server.get('/compliment', function(req,res){
+  console.log(res, req)
+  res.send('hello')
+})
+
+server.listen(3000)
+
 
 module.exports = server
