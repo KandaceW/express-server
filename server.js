974ab422fbd46dae0daf2ef31bfac69a2d17ce59
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-05-31 11:01:11 +1200

    add a sendFile

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ server.get("/compliment", function(req, res) {
   res.send("I like your feet.");
 });
 
+server.get("/profile", function(req, res) {
+  res.sendFile(__dirname + "/silvia.html");
+});
+
 module.exports = server;
