1eed32c7655e91c0a3cfa80dfc9e880639000c06
Daniel Bridewell <48977884+daniel-bridewell@users.noreply.github.com>
2019-05-31 11:00:30 +1200

    compliment

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const server = express()
 
+server.get('/compliment', function (req, res) {
+  res.send('<h1 style= "color: blue">I like ur shirt :)</h1>')
+})
+
 module.exports = server
