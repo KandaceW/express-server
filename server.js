d200cff7a1379d2682f47321044f2631622c1e11
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-31 11:07:09 +1200

    first go

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require('express')
 const server = express()
 
 module.exports = server
+
+server.get('/compliment', (req, res) => {
+res.send('<marquee><h1>HELLO PARTY PEOPLE!!!!!!!!!!!!!!!!!!</h1></marquee>') 
+
+})
