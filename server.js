f77ad4cfd9260f7189dc434a5e39a7bf630dc67a
OliverPetri <49376686+OliverPetri@users.noreply.github.com>
2019-05-31 11:51:45 +1200

    Second Commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ server.get ('/compliment',function(reg,res){
     res.send ('<h3>Great to see you!</h3>')
 })
 
+server.get ('/profile',function(reg,res){
+    res.sendFile(__dirname+"/silvia.html")
+})
+
 
 
 module.exports = server
