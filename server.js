7bebb4c798df4c60fcf3936e5ad0561b4786968f
Daniel Bridewell <48977884+daniel-bridewell@users.noreply.github.com>
2019-05-31 11:21:47 +1200

    add profile

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const server = express()
 
+const path = require('path')
+
 server.get('/compliment', function (req, res) {
   res.send('<h1 style= "color: blue">I like ur shirt :)</h1>')
+})
+
+server.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, "silvia.html"))
 })
 
 module.exports = server
