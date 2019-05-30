cf58878584671eb987b77e288415c8d4a5f423c1
OliverPetri <49376686+OliverPetri@users.noreply.github.com>
2019-05-31 11:18:48 +1200

    First Commit

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const server = express()
 
+server.get ('/compliment',function(reg,res){
+    res.send ('<h3>Great to see you!</h3>')
+})
+
+
+
 module.exports = server
