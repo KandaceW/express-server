87f500e03f89e5dce7d8342d483a1fb699d7790a
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-31 12:38:42 +1200

    add profile page and nodemon

--- server.js
+++ server.js
@@ -7,13 +7,21 @@ server.get('/compliment', function (req, res) {
 })
 
 server.get('/profile', function (req, res) {
-  // res.send('Profile Page')
   var name = ''
-  
+
   if (req.query.name == 'silvia') {
     res.sendFile(__dirname + "/silvia.html")
   }
   if (req.query.name == 'sampson') {
+    res.sendFile(__dirname + "/sampson.html")
+  }
+})
+
+server.get('/profiles/:id', function (req, res) {
+  if (req.params.id == 1) {
+    res.sendFile(__dirname + "/silvia.html")
+  }
+  if (req.param.id == 2) {
     res.sendFile(__dirname + "/sampson.html")
   }
 })
