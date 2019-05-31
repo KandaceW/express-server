da0a2538592d9f164b42326640c139fa3f213920
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-31 16:36:03 +1200

    Post Data to the server (updated)

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
+
 server.get('/compliment', function (req, res) {
   res.send('Hi from Tom and Dan')
 })
@@ -26,8 +29,17 @@ server.get('/profiles/:id', function (req, res) {
   if (req.params.id == 2) {
     res.sendFile(__dirname + "/sampson.html")
   }
+  if (req.params.id == 3) {
+    res.sendFile(__dirname + "/public/get-name.html")
+  }
 })
 
-server.use(express.static('public'))
 
-module.exports = server
+server.post('/named-compliment', function (req, res) {
+  console.log(req.body)
+  var name = req.body.name
+
+  res.send("<h1>" + name + ", thanks for your support!</h1>")
+})
+
+module.exports = server;
