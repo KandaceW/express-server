366939643ce4ff283e9284a0a6c668e41fab09fc
Quentin Papera <49025456+quentin-papera@users.noreply.github.com>
2019-05-31 16:43:35 +1200

    dan&john

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.static('public'))
+server.use(express.urlencoded({extended:true}))
+
 module.exports = server
 
 const path = require('path')
@@ -17,7 +20,24 @@ server.get('/profile', function(req, res){
     } else if(name == 'silvia'){
         res.sendFile(path.join(__dirname, 'silvia.html'))
     }
+})
 
+server.get('/profiles/:id', function(req, res){
+     var id = req.params.id
+     if(id == 1) {
+        res.sendFile(path.join(__dirname, 'silvia.html'))
+     } else if(id == 2){
+        res.sendFile(path.join(__dirname, 'sampson.html'))
+     }
+}) 
+
+server.get("/get-name", function(req, res){
+    res.sendFile(path.join(__dirname, 'get-name.html'))
+   
+})
+
+server.post ("/named-compliment", function(req, res){
+    console.log(req.body)
 })
 
 
