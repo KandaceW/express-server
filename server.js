49c8ce3f949b62141aed59262f4f3aa7124ba3a3
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-05-31 12:31:50 +1200

    add a sendFile

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.static("public"));
+
 server.get("/compliment", function(req, res) {
   res.send("I like your feet.");
 });
 
 server.get("/profile", function(req, res) {
-  var name = "";
-
+  var name = ""
   if (req.query.name == "silvia") {
+    console.log("name", name);
     res.sendFile(__dirname + "/silvia.html");
   }
   if (req.query.name == "sampson") {
@@ -17,13 +19,13 @@ server.get("/profile", function(req, res) {
   }
 });
 
-server.get('/profiles/:id', function(req, res, id){
+server.get("/profiles/:id", function(req, res) {
   if (req.params.id == 1) {
     res.sendFile(__dirname + "/silvia.html");
   }
   if (req.params.id == 2) {
     res.sendFile(__dirname + "/sampson.html");
   }
-})
+});
 
 module.exports = server;
