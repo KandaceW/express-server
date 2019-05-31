0db4fb592f1bbb547e33e2734a43583352811524
OliverPetri <49376686+OliverPetri@users.noreply.github.com>
2019-05-31 16:12:17 +1200

    Third Commit

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require('express')
 
+// links to express server
+const express = require('express')
+// calling(function) the server var to link to server
 const server = express()
 
-server.get ('/compliment',function(reg,res){
+// server.get finds path and runs function and sends file to server
+server.get ('/compliment',function(req,res){
     res.send ('<h3>Great to see you!</h3>')
 })
 
-server.get ('/profile',function(reg,res){
-    res.sendFile(__dirname+"/silvia.html")
+server.get ('/profile/:name',function(req,res){
+    var name = req.params.name
+
+    if(name == 'profile'){
+        res.sendFile(__dirname+"/silvia.html")
+    
+    } else if(name == 'about') {
+        res.sendFile(__dirname+"/sampson.html")
+    }
 })
+
+// server.get ('/about',function(reg,res){
+//     res.sendFile(__dirname+"/sampson.html")
+
+
 
 
 
