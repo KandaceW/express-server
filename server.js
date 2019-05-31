9f2fac904ac5070b7f664e90e994796a2d497a51
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-05-31 12:25:53 +1200

    it works ; git push origin

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.static('public'))
+
 const path = require('path')
+
 
 server.get('/compliment', function (req, res) {
   res.send('you have wonderful feet')
 })
 
+
 server.get('/profile', function (req, res) {
   var name = req.query.name
-  
+
   if (name == 'silvia') {
     res.sendFile(path.join(__dirname, 'silvia.html'))
   }
@@ -24,27 +28,15 @@ server.get('/profile', function (req, res) {
 server.get('/profiles/:id', function (req, res) {
   const id = req.params.id
 
-  const users = {
-    1: 'silvia',
-    2: 'sampson'
-  }
-
-  const name = users[id]
-
-  if (name == 'silvia') {
+  if (id == 1) {
     res.sendFile(path.join(__dirname, 'silvia.html'))
   }
-  else if (name == 'sampson') {
+  else if (id == 2) {
     res.sendFile(path.join(__dirname, 'sampson.html'))
   } else {
     res.send('you have wonderful feet')
   }
 })
-
-// server.get('/profile', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'silvia.html')) 
-//   })
-
 
 module.exports = server
 
