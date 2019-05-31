90ebabf145c6c3a9a8bd255413ec51d2e4120c4c
Ben Sherwood <48200039+ben-sherwood@users.noreply.github.com>
2019-05-31 12:06:39 +1200

    Sendfile working

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require('express')
 const server = express()
 var PORT = 3000
 
-server.get('/compliment', function(req,res){
-  console.log(res, req)
+server.get('/compliment', function (req, res) {
   res.send('hello')
 })
 
-server.listen(3000)
+server.get('/filetest', function (req, res) {
+
+  res.sendFile('/home/student/workspace/express-server/silvia.html')
+})
+
+server.listen(PORT)
+
 
 
 module.exports = server
