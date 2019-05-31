6ce3c96bdf9e362b98daf2aecd3ef5400fdde15c
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-31 12:08:51 +1200

    I am sylvia

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 
 const server = express()
 
-module.exports = server
+
 
 server.get('/compliment', (req, res) => {
-res.send('<marquee><h1>HELLO PARTY PEOPLE!!!!!!!!!!!!!!!!!!</h1></marquee>') 
-
+  res.send('<marquee><h1>HELLO PARTY PEOPLE!!!!!!!!!!!!!!!!!!!</h1></marquee>') 
 })
+
+server.get('/profile', (req, res) => {
+  const name = req.query.name
+  res.sendFile(__dirname +'/silvia.html')
+// if (!name) res.redirect('/compliment')
+})
+
+module.exports = server
