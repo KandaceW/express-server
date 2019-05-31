394ca3ae91c3376f844b82c043f4c578c1a567ca
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-05-31 15:43:42 +1200

    post a from entry

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const express = require("express");
 const server = express();
 
 server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
 
 server.get("/compliment", function(req, res) {
-  res.send("I like your feet.");
+  // res.send("I like your feet.");
+  res.sendFile(__dirname + "/public/get-name.html");
+});
+
+server.post("/named-compliment", function(req, res) {
+  //console.log(req.body);
+  res.send("<h2>"+req.body.name+" you have great fingernails</h2>");
 });
 
 server.get("/profile", function(req, res) {
-  var name = ""
+  var name = "";
   if (req.query.name == "silvia") {
     console.log("name", name);
     res.sendFile(__dirname + "/silvia.html");
@@ -26,6 +33,10 @@ server.get("/profiles/:id", function(req, res) {
   if (req.params.id == 2) {
     res.sendFile(__dirname + "/sampson.html");
   }
+});
+
+server.get("/named-compliment", function(req, res) {
+  res.sendFile(__dirname + "I like your feet.");
 });
 
 module.exports = server;
