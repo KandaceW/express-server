d0e07a9fb659c6e8fe54d25f8a002e7251de0074
mikeywipo <49007478+mikeywipo@users.noreply.github.com>
2019-05-31 12:38:43 +1200

    update file

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ server.get('/compliment', function (req, res) {
   res.send('<p>Hi there</p>')
 })
 
+server.get('/profile', function (req, res) {
+  var name = ''
+  
+  if (req.query.name == ('silvia') {
+  res.sendFile(__dirname + '/silvia.html')
+  }
+})
+
+
+
 module.exports = server
