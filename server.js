0ecf63039c4f014891da933766e64db13230fe97
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-05-31 12:05:47 +1200

    add URL parameter

--- server.js
+++ server.js
@@ -10,12 +10,34 @@ server.get('/compliment', function (req, res) {
 
 server.get('/profile', function (req, res) {
   var name = req.query.name
-  console.log(name)
+  
   if (name == 'silvia') {
     res.sendFile(path.join(__dirname, 'silvia.html'))
   }
   else if (name == 'sampson') {
     res.sendFile(path.join(__dirname, 'sampson.html'))
+  } else {
+    res.send('you have wonderful feet')
+  }
+})
+
+server.get('/profiles/:id', function (req, res) {
+  const id = req.params.id
+
+  const users = {
+    1: 'silvia',
+    2: 'sampson'
+  }
+
+  const name = users[id]
+
+  if (name == 'silvia') {
+    res.sendFile(path.join(__dirname, 'silvia.html'))
+  }
+  else if (name == 'sampson') {
+    res.sendFile(path.join(__dirname, 'sampson.html'))
+  } else {
+    res.send('you have wonderful feet')
   }
 })
 
