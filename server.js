2d73fe17547607fa69f2ad98f0fd5cd6e5730c4a
mikeywipo <49007478+mikeywipo@users.noreply.github.com>
2019-05-31 15:15:35 +1200

    adding silvia and sampson htmls

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ server.get('/compliment', function (req, res) {
 server.get('/profile', function (req, res) {
   var name = ''
   
-  if (req.query.name == ('silvia') {
-  res.sendFile(__dirname + '/silvia.html')
+  if (req.query.name == 'silvia') {
+  res.sendFile(__dirname + "/silvia.html")
+  }
+  if (req.query.name == 'sampson') {
+  res.sendFile(__dirname + "/sampson.html")
   }
 })
 
+server.get('/profiles/:id', function (req, res) {
 
+})
 
 module.exports = server
