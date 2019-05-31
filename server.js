884bee5b630e53ac90bc5ec88ccd01ffd3a01ca4
Ben Sherwood <48200039+ben-sherwood@users.noreply.github.com>
2019-05-31 12:50:10 +1200

    Test 3 complete

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const server = express()
 var PORT = 3000
 
 server.get('/compliment', function (req, res) {
+  
   res.send('hello')
-})
+});
 
-server.get('/filetest', function (req, res) {
+server.get('/profile', function (req, res) {
+  if (req.query.name == "silvia"){
+    res.sendFile('/home/student/workspace/express-server/silvia.html')
+  } else if(req.query.name == "sampson"){
+    res.sendFile('/home/student/workspace/express-server/sampson.html')
+  }
+  
+});
 
-  res.sendFile('/home/student/workspace/express-server/silvia.html')
-})
+
+
+
 
 server.listen(PORT)
 
