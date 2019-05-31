00236aa920194806634f09e63efb22ecc2bb36d1
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-31 16:30:09 +1200

    add profiles id

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require('express')
+const express = require("express");
 
-const server = express()
+const server = express();
+
+server.get("/compliment", (req, res) => {
+  res.send("<marquee><h1>HELLO PARTY PEOPLE!!!!!!!!!!!!!!!!!!!</h1></marquee>");
+});
+
+server.get("/profile/:name", (req, res) => {
+  const name = req.params.name;
+
+  if (name === "Sylvia") {
+    res.sendFile(__dirname + "/sylvia.html");
+    // res.redirect("/sampson.html");
+  } else if (name === "Sampson") {
+    res.sendFile(__dirname + "/sampson.html");
+  } else {
+    res.send("Nope");
+  }
+
+  server.get('/profiles/:id', (req, res) => {
+    const id = req.params.id
+
+    const users = {
+      1: 'sylvia',
+      2: 'sampson'
+    }
+    res.sendFile("YO")
+  }),
+  // const filePath = path.join(__dirname, "public", name.toLowerCase() + ".html");
+  // res.sendFile(filePath);
+  // })
 
 
 
-server.get('/compliment', (req, res) => {
-  res.send('<marquee><h1>HELLO PARTY PEOPLE!!!!!!!!!!!!!!!!!!!</h1></marquee>') 
-})
-
-server.get('/profile', (req, res) => {
-  const name = req.query.name
-  res.sendFile(__dirname +'/silvia.html')
-// if (!name) res.redirect('/compliment')
-})
 
 module.exports = server
